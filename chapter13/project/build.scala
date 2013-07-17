17205df234a5d4b313da5f00fcff93dca08435fd
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-07-17 19:06:54 +0200

    Re-organized build file a bit.

--- chapter13/project/build.scala
+++ chapter13/project/build.scala
@@ -12,23 +12,22 @@ object Chapter13Build extends Build {
   val ScalaVersion = "2.10.2"
   val ScalatraVersion = "2.2.1"
 
-  lazy val project = Project (
-    "chapter13",
-    file("."),
-    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+  val myDependencies = Seq(
+      "org.scalatra" %% "scalatra" % ScalatraVersion,
+      "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+      "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+      "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+      "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+      "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+    )
+
+  val mySettings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
-      libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
-      ),
+      libraryDependencies ++= myDependencies,
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
           TemplateConfig(
@@ -42,5 +41,6 @@ object Chapter13Build extends Build {
         )
       }
     )
-  )
+
+  lazy val project = Project("chapter13", file("."), settings = mySettings)
 }
