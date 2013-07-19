c5f0033b9dbddb25af1e8bbeb865a3bf0ff5b212
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-07-19 19:58:27 +0200

    Added production config for backlog.

--- chapter13/project/build.scala
+++ chapter13/project/build.scala
@@ -30,7 +30,7 @@ object Chapter13Build extends Build {
       "org.scalatra" %% "scalatra" % ScalatraVersion,
       "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
       "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-      "ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime",
+      "ch.qos.logback" % "logback-classic" % "1.0.13",
       "com.typesafe" % "config" % "1.0.2",
       "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
       "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
