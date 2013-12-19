08e3d5fe5fb3bce186cc1f77ade61ed2fabf5391
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-12-19 03:23:46 +0100

    Added id to comment.

--- comments-collector/src/main/scala/comments/data.scala
+++ comments-collector/src/main/scala/comments/data.scala
@@ -3,14 +3,14 @@ package comments
 import com.mongodb.casbah.Imports._
 import org.bson.types.ObjectId
 
-case class Comment(url: String, title: String, body: String)
+case class Comment(id: String, url: String, title: String, body: String)
 
 case class CommentsRepository(collection: MongoCollection) {
 
-  def create(url: String, title: String, body: String): ObjectId = {
+  def create(url: String, title: String, body: String): String = {
     val m = MongoDBObject("url" -> url, "title" -> title, "body" -> body)
     collection += m
-    m.getAs[ObjectId]("_id").get
+    m.getAs[ObjectId]("_id").get.toString
   }
 
   def findAll: List[Comment] = {
@@ -22,9 +22,10 @@ case class CommentsRepository(collection: MongoCollection) {
   }
 
   protected def toComment(db: DBObject): Option[Comment] = for {
+    id <- db.getAs[ObjectId]("_id")
     u <- db.getAs[String]("url")
     t <- db.getAs[String]("title")
     b <- db.getAs[String]("body")
-  } yield Comment(u, t, b)
+  } yield Comment(id.toString, u, t, b)
 
 }
