87add6e30a9befe89faf36093a3f57093e3d44ba
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-01-26 16:27:55 +0000

    We'll deal with Atmosphere later in the book.

--- chapter02/src/main/scala/ScalatraBootstrap.scala
+++
@@ -1,9 +0,0 @@
-import com.example.chat._
-import org.scalatra._
-import javax.servlet.ServletContext
-
-class ScalatraBootstrap extends LifeCycle {
-  override def init(context: ServletContext) {
-    context.mount(new ChatController, "/*")
-  }
-}
