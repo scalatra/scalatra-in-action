2a8779740ad6c8a8c1a86b5a3ed8be0ece402db3
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-08 15:26:19 +0000

    Swaggerizing ScalatraBootstrap.

--- chapter11/hacker-tracker-api/src/main/scala/ScalatraBootstrap.scala
+++ chapter11/hacker-tracker-api/src/main/scala/ScalatraBootstrap.scala
@@ -1,15 +1,27 @@
 import com.constructiveproof.hackertracker._
 import com.constructiveproof.hackertracker.init.DatabaseInit
-import org.scalatra._
 import javax.servlet.ServletContext
+import org.scalatra._
+import org.scalatra.swagger.{ApiInfo, Swagger}
 
 class ScalatraBootstrap extends LifeCycle with DatabaseInit {
 
   override def init(context: ServletContext) {
+    implicit val apiInfo = new ApiInfo(
+      "The HackerTracker API",
+      "Docs for the HackerTracker API",
+      "http://www.constructiveproof.com/hacker-tracker/tos.html",
+      "Dave Hrycyszyn",
+      "MIT",
+      "http://constructiveproof.com/hacker-tracker/api/license.html")
+
+    implicit val swagger = new Swagger("1.0", "1", apiInfo)
+
     configureDb()
     context.mount(new ApiController, "/api/hackers")
     context.mount(new DatabaseSetupController, "/database")
     context.mount(new HackersController, "/hackers")
+    context.mount(new HackersSwagger, "/api-docs/*")
     context.mount(new SessionsController, "/sessions")
   }
 
