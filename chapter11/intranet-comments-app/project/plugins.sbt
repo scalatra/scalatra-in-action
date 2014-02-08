eb831d3e66b1c41828f6f90dfccadbcedfdc4531
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-08 12:13:34 +0000

    Removing comments app.

--- chapter11/intranet-comments-app/project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("com.mojolly.scalate" % "xsbt-scalate-generator" % "0.4.2")
-
-addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.2")
--- chapter11/intranet-comments-app/src/main/scala/com/example/comments/models/Models.scala
+++
@@ -1,4 +0,0 @@
-package com.example.comments.models
-
-// A Comment object to use as a data model
-case class Comment(url: String, title: String, body: String)
