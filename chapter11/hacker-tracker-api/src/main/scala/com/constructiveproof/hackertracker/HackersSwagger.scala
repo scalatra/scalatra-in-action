dfc582a2468d7f48cadd4390d10bc3be475c2cec
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-09 13:26:33 +0000

    Adding in necessary DefaultFormats

--- chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/HackersSwagger.scala
+++ chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/HackersSwagger.scala
@@ -2,7 +2,10 @@ package com.constructiveproof.hackertracker
 
 import org.scalatra.swagger.{Swagger, JacksonSwaggerBase}
 import org.scalatra.ScalatraServlet
+import org.json4s.DefaultFormats
 
-class HackersSwagger(implicit val swagger: Swagger) extends ScalatraServlet with JacksonSwaggerBase {
+class HackersSwagger(implicit val swagger: Swagger)
+  extends ScalatraServlet with JacksonSwaggerBase {
 
+  override protected implicit val jsonFormats = DefaultFormats
 }
