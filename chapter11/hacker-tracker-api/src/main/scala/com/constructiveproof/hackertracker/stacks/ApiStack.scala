3fd1a603e07d15f8ccfb0b1537c586c911b301b2
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-09 13:21:13 +0000

    Adding SwaggerSupport to ApiStack

--- chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/stacks/ApiStack.scala
+++ chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/stacks/ApiStack.scala
@@ -1,9 +1,9 @@
 package com.constructiveproof.hackertracker.stacks
 
 import org.scalatra.json.JacksonJsonSupport
-import org.scalatra.swagger.JacksonSwaggerBase
+import org.scalatra.swagger.{SwaggerSupport, JacksonSwaggerBase}
 
 trait ApiStack extends HackerCoreStack
-  with JacksonJsonSupport with JacksonSwaggerBase{
+  with JacksonJsonSupport with JacksonSwaggerBase {
 
 }
