50dff514a20f4fb13b8090c45e7dff8bd7f1bdda
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-11 14:34:04 +0000

    Documented list, create and get actions for Hackers.

--- chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/ApiController.scala
+++ chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/ApiController.scala
@@ -3,7 +3,9 @@ package com.constructiveproof.hackertracker
 import com.constructiveproof.hackertracker.models.Hacker
 import com.constructiveproof.hackertracker.stacks.ApiStack
 import org.scalatra.BadRequest
-import org.scalatra.swagger.{Swagger, SwaggerSupport}
+import org.scalatra.swagger._
+import org.scalatra.swagger.Parameter
+import scala.Some
 
 class ApiController()(implicit val swagger: Swagger) extends ApiStack with SwaggerSupport {
 
@@ -11,17 +13,45 @@ class ApiController()(implicit val swagger: Swagger) extends ApiStack with Swagg
     contentType = formats("json")
   }
 
+  // Identifies the controller to swagger
+  override protected val applicationName = Some("hackers")
+  protected val applicationDescription =
+    """The Hacker Tracker API. Exposes operations for adding hackers and retrieving lists of hackers."""
+
+  val listHackers = (apiOperation[List[Hacker]]("listHackers")
+    summary("Show all hackers")
+    notes("Shows all available hackers."))
+
+  val createHacker = (apiOperation[Hacker]("createHacker")
+    summary("Create a new hacker")
+    notes("firstname, lastname, motto, and year of birth are required")
+    parameters(
+      Parameter("firstname", DataType.String, Some("The hacker's first name"), None, ParamType.Body, required = true),
+      Parameter("lastname", DataType.String, Some("The hacker's last name"), None, ParamType.Body, required = true),
+      Parameter("motto", DataType.String, Some("A phrase associated with this hacker"), None, ParamType.Body, required = true),
+      Parameter("birthyear", DataType.Int, Some("A four-digit number, the year that the user was born in"),
+        Some("A four-digit number"), ParamType.Body, required = true))
+    )
+
+  val getHacker = (apiOperation[Hacker]("getHacker")
+    summary("Retrieve a single hacker by id")
+    notes("Foo")
+    parameters(
+      Parameter("id", DataType.Int, Some("The hacker's database id"), None, ParamType.Path, required = true)
+    ))
+
+
   /**
    * List all hackers.
    */
-  get("/") {
+  get("/", operation(listHackers)) {
     Hacker.all.toList
   }
 
   /**
    * Retrieve a specific hacker.
    */
-  get("/:id") {
+  get("/:id", operation(getHacker)) {
     val id = params.getAs[Int]("id").getOrElse(0)
     Hacker.get(id)
   }
@@ -29,7 +59,7 @@ class ApiController()(implicit val swagger: Swagger) extends ApiStack with Swagg
   /**
    * Create a new hacker in the database.
    */
-  post("/") {
+  post("/", operation(createHacker)) {
     val firstName = params("firstname")
     val lastName = params("lastname")
     val motto = params("motto")
