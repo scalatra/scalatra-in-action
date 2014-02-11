c81d660b67e61b5ea934d8d47c4db1fc23f28611
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-11 15:00:34 +0000

    This json4s stuff is really necessary here.

--- chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/stacks/ApiStack.scala
+++ chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/stacks/ApiStack.scala
@@ -2,8 +2,11 @@ package com.constructiveproof.hackertracker.stacks
 
 import org.scalatra.json.JacksonJsonSupport
 import org.scalatra.swagger.{SwaggerSupport, JacksonSwaggerBase}
+import org.json4s.DefaultFormats
 
 trait ApiStack extends HackerCoreStack
   with JacksonJsonSupport with JacksonSwaggerBase {
+
+  override protected implicit val jsonFormats = DefaultFormats
 
 }
