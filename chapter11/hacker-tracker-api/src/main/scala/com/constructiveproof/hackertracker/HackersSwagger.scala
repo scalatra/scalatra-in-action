78d4f26379d0dea021fc4d313682daa91e2cb16c
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-15 21:49:21 +0000

    Import cleanup.

--- chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/HackersSwagger.scala
+++ chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/HackersSwagger.scala
@@ -2,7 +2,6 @@ package com.constructiveproof.hackertracker
 
 import org.scalatra.swagger.{Swagger, JacksonSwaggerBase}
 import org.scalatra.ScalatraServlet
-import org.json4s.DefaultFormats
 
 class HackersSwagger(implicit val swagger: Swagger)
   extends ScalatraServlet with JacksonSwaggerBase {
