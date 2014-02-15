80a54f14f718e509888463a19ca8f0031f7e83ee
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-02-15 21:49:40 +0000

    Put the Swagger docs closer to corresponding methods.

--- chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/ApiController.scala
+++ chapter11/hacker-tracker-api/src/main/scala/com/constructiveproof/hackertracker/ApiController.scala
@@ -1,11 +1,12 @@
 package com.constructiveproof.hackertracker
 
+import scala.Some
+
 import com.constructiveproof.hackertracker.models.Hacker
 import com.constructiveproof.hackertracker.stacks.ApiStack
 import org.scalatra.BadRequest
 import org.scalatra.swagger._
-import org.scalatra.swagger.Parameter
-import scala.Some
+import com.constructiveproof.hackertracker.auth.ApiAuthenticationSupport
 
 class ApiController()(implicit val swagger: Swagger) extends ApiStack with SwaggerSupport {
 
@@ -15,7 +16,7 @@ class ApiController()(implicit val swagger: Swagger) extends ApiStack with Swagg
   }
 
   // Identifies the controller to swagger
-//  override protected val applicationName = Some("hackers")
+  override protected val applicationName = Some("hackers")
   protected val applicationDescription =
     """The Hacker Tracker API. Exposes operations for adding hackers and retrieving lists of hackers."""
 
@@ -23,43 +24,35 @@ class ApiController()(implicit val swagger: Swagger) extends ApiStack with Swagg
     summary("Show all hackers")
     notes("Shows all available hackers."))
 
-  val createHacker = (apiOperation[Hacker]("createHacker")
-    summary("Create a new hacker")
-    notes("firstname, lastname, motto, and year of birth are required")
-    parameters(
-      Parameter("firstname", DataType.String, Some("The hacker's first name"), None, ParamType.Body, required = true),
-      Parameter("lastname", DataType.String, Some("The hacker's last name"), None, ParamType.Body, required = true),
-      Parameter("motto", DataType.String, Some("A phrase associated with this hacker"), None, ParamType.Body, required = true),
-      Parameter("birthyear", DataType.Int, Some("A four-digit number, the year that the user was born in"),
-        Some("A four-digit number"), ParamType.Body, required = true))
-    )
+  get("/", operation(listHackers)) {
+    Hacker.all.toList
+  }
+
 
   val getHacker = (apiOperation[Hacker]("getHacker")
     summary("Retrieve a single hacker by id")
     notes("Foo")
     parameters(
-      Parameter("id", DataType.Int, Some("The hacker's database id"), None, ParamType.Path, required = true)
+    Parameter("id", DataType.Int, Some("The hacker's database id"), None, ParamType.Path, required = true)
     ))
 
-
-  /**
-   * List all hackers.
-   */
-  get("/", operation(listHackers)) {
-    Hacker.all.toList
-  }
-
-  /**
-   * Retrieve a specific hacker.
-   */
   get("/:id", operation(getHacker)) {
     val id = params.getAs[Int]("id").getOrElse(0)
     Hacker.get(id)
   }
 
-  /**
-   * Create a new hacker in the database.
-   */
+
+  val createHacker = (apiOperation[Hacker]("createHacker")
+    summary("Create a new hacker")
+    notes("firstname, lastname, motto, and year of birth are required")
+    parameters(
+    Parameter("firstname", DataType.String, Some("The hacker's first name"), None, ParamType.Body, required = true),
+    Parameter("lastname", DataType.String, Some("The hacker's last name"), None, ParamType.Body, required = true),
+    Parameter("motto", DataType.String, Some("A phrase associated with this hacker"), None, ParamType.Body, required = true),
+    Parameter("birthyear", DataType.Int, Some("A four-digit number, the year that the user was born in"),
+      Some("A four-digit number"), ParamType.Body, required = true))
+    )
+
   post("/", operation(createHacker)) {
     val firstName = params("firstname")
     val lastName = params("lastname")
