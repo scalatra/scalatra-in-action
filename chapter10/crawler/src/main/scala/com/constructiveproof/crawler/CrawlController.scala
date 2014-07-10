6aa06aab5c804971fe0e8ec1b35854987c7c6ad3
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-07-10 21:42:46 +0100

    Scaling back to a synchronous app.

--- chapter10/crawler/src/main/scala/com/constructiveproof/crawler/CrawlController.scala
+++ chapter10/crawler/src/main/scala/com/constructiveproof/crawler/CrawlController.scala
@@ -3,31 +3,25 @@ package com.constructiveproof.crawler
 import java.net.URL
 import java.nio.charset.StandardCharsets
 
-import org.scalatra._
-
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.io.Source
 
-class CrawlController extends CrawlerStack with FutureSupport {
+class CrawlController extends CrawlerStack {
 
   protected implicit def executor: ExecutionContext = ExecutionContext.global
 
   get("/") {
     contentType = "text/html"
-    new AsyncResult {
-      val is = Grabber.grab(new URL("https://constructiveproof.com"))
-    }
+    Grabber.grab(new URL("https://constructiveproof.com"))
   }
 }
 
 object Grabber {
 
-  def grab(url: URL)(implicit ctx: ExecutionContext): Future[String] = {
-    Future {
-      Source.fromURL(
-        new URL("https://constructiveproof.com"), StandardCharsets.UTF_8.name()
-      ).mkString
-    }
+  def grab(url: URL)(implicit ctx: ExecutionContext): String = {
+    Source.fromURL(
+      new URL("https://constructiveproof.com"), StandardCharsets.UTF_8.name()
+    ).mkString
   }
 
 }
