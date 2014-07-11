25481353cf7dbc669b6804a342131485b0e30398
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-07-11 21:23:43 +0100

    Compiling AkkaCrawler.

--- chapter10/crawler/src/main/scala/com/constructiveproof/crawler/AkkaCrawler.scala
+++ chapter10/crawler/src/main/scala/com/constructiveproof/crawler/AkkaCrawler.scala
@@ -2,16 +2,23 @@ package com.constructiveproof.crawler
 
 import java.net.URL
 
-import akka.pattern.ask
 import akka.actor.{ActorRef, ActorSystem}
+import akka.pattern.ask
+import akka.util.Timeout
 import org.scalatra.AsyncResult
+import scala.concurrent.duration._
+
+import scala.concurrent.ExecutionContext
 
 /**
  * Created by dave on 11/07/2014.
  */
 class AkkaCrawler(system: ActorSystem, grabActor: ActorRef) extends CrawlerStack {
 
-  
+
+  protected implicit def executor: ExecutionContext = system.dispatcher
+
+  implicit val defaultTimeout = new Timeout(2 seconds)
 
   get("/") {
     contentType = "text/html"
