46350ab7091a97258dd57f88b0d4224f97052ebb
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-07-11 21:12:59 +0100

    A working Async sample

--- chapter10/crawler/src/main/scala/com/constructiveproof/crawler/CrawlController.scala
+++ chapter10/crawler/src/main/scala/com/constructiveproof/crawler/CrawlController.scala
@@ -3,26 +3,35 @@ package com.constructiveproof.crawler
 import java.net.URL
 import java.nio.charset.StandardCharsets
 
+import org.scalatra.{AsyncResult, FutureSupport}
+
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.Source
 
-class CrawlController extends CrawlerStack {
+
+class CrawlController extends CrawlerStack with FutureSupport {
+
+  protected implicit def executor = ExecutionContext.global
 
   get("/") {
     contentType = "text/html"
-    Grabber.evaluate(new URL(params("url")))
+    new AsyncResult { val is =
+      Grabber.evaluate(new URL(params("url")))
+    }
   }
 }
 
 object Grabber {
-
-  def evaluate(url: URL): String = {
-    val content = Source.fromURL(
-      url, StandardCharsets.UTF_8.name()
-    ).mkString
-    content.contains("Scala") match {
-      case true => "It's a Scala site, very cool."
-      case false => "Whoops, you've made some sort of mistake in your reading choices."
+  def evaluate(url: URL)(implicit ctx: ExecutionContext): Future[String] = {
+    Future {
+      val content = Source.fromURL(
+        url, StandardCharsets.UTF_8.name()
+      ).mkString
+      content.contains("Scala") match {
+        case true => "It's a Scala site, very cool."
+        case false => "Whoops, you've made some sort " +
+          "of mistake in your reading choices."
+      }
     }
   }
-
 }
