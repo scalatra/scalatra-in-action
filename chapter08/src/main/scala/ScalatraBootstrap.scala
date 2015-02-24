8ba0fc9b105a183483d6ae98d2c201826205ba1f
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-02-25 00:27:42 +0100

    Aligned code to chapters.

--- chapter08/src/main/scala/ScalatraBootstrap.scala
+++ chapter08/src/main/scala/ScalatraBootstrap.scala
@@ -5,6 +5,7 @@ import org.scalatra.book.chapter07._
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    context.mount(new GreeterServlet, "/*")
+    context.mount(new MyScalatraServlet, "/*")
+    context.mount(new NukeLauncherServlet(RealNukeLauncher), "/nuke/*")
   }
 }
