f5df09b1a394e1ffee1b360d98dc0f398a5b8e85
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-02-25 23:05:14 +0100

    Updated code for chapter 9.

--- chapter09/src/main/scala/org/scalatra/book/chapter09/Chapter09.scala
+++ chapter09/src/main/scala/org/scalatra/book/chapter09/Chapter09.scala
@@ -1,9 +1,27 @@
 package org.scalatra.book.chapter09
 
-class Chapter09 extends Chapter09Stack {
+import org.scalatra.ScalatraServlet
+import org.scalatra.scalate.ScalateSupport
 
-  get("/?") {
-    f"Hello, from action! (isDevelopment = ${isDevelopmentMode}, serverHost = ${serverHost}, forceSsl = ${needsHttps}})"
+class Chapter09(appConfig: AppConfig) extends ScalatraServlet with ScalateSupport {
+
+  get("/") {
+    f"Hello, from action! (isDevelopment = ${isDevelopmentMode}})"
+  }
+
+  get("/shorten-url") {
+    val token = scala.util.Random.nextString(8)
+    f"${appConfig.webBase}/$token"
+  }
+
+  notFound {
+    // remove content type in case it was set through an action
+    contentType = null
+    // Try to render a ScalateTemplate if no route matched
+    findTemplate(requestPath) map { path =>
+      contentType = "text/html"
+      layoutTemplate(path)
+    } orElse serveStaticResource() getOrElse resourceNotFound()
   }
 
 }
