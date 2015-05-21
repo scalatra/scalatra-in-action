4151b25825b9d0e6ec069bda4248860bf30a1ad9
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2015-05-21 13:45:05 +0100

    Renaming build.scala to be lower case, as most others are.

---
+++ chapter09/project/build.scala
@@ -0,0 +1,53 @@
+import sbt._
+import Keys._
+
+import org.scalatra.sbt._
+
+import com.mojolly.scalate._
+import com.mojolly.scalate.ScalatePlugin._
+import com.mojolly.scalate.ScalatePlugin.ScalateKeys._
+
+object Chapter09Build extends Build {
+  val Organization = "org.scalatra"
+  val Name = "Chapter 9"
+  val Version = "0.1.0-SNAPSHOT"
+  val ScalaVersion = "2.11.4"
+  val ScalatraVersion = "2.4.0.M2"
+
+  val mySettings =
+    ScalatraPlugin.scalatraSettings ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers += Classpaths.typesafeReleases,
+      libraryDependencies ++= Seq(
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "com.typesafe" % "config" % "1.0.2",
+        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided")
+    )
+
+  val myScalateSettings =
+    ScalatePlugin.scalateSettings ++ Seq(
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) { base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty, /* default imports should be added here */
+            Seq(
+              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+            ), /* add extra bindings here */
+            Some("templates")
+          )
+        )
+      }
+    )
+
+  lazy val project = Project("chapter09", file("."))
+    .settings(mySettings: _*)
+    .settings(myScalateSettings: _*)
+
+}
