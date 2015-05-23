383f26ebc04e1e2a4f71106f12cd85bebb8a0eee
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2015-05-23 15:08:32 +0100

    Selectively building projects as some aren't working yet.

--- buildall.sh
+++ buildall.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Run the test suite for each project.
-for SCRIPT in `find . -name sbt -type f -perm -u=x`
+for SCRIPT in `find . -name .test -type f -perm -u=w`
 do
     OLD_PWD=`pwd`
     PROJ=`dirname $SCRIPT`
@@ -11,6 +11,6 @@ do
     echo "BUILDING $PROJ                                          "
     echo '########################################################'
     cd $PROJ
-    ./sbt test
+    sbt test
     cd $OLD_PWD
 done
