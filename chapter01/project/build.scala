4e1bb750a3e1e3bb7c7e568ad93bacc0cc57ba39
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-05-26 19:43:59 +0200

    Updated build.

--- chapter01/project/build.scala
+++ chapter01/project/build.scala
@@ -11,12 +11,11 @@ object MyScalatraWebAppBuild extends Build {
   val Version = "0.1.0-SNAPSHOT"
   val ScalaVersion = "2.11.6"
   val ScalatraVersion = "2.4.0.RC1"
-  val JettyVersion = "9.1.3.v20140225"
 
   lazy val project = Project (
     "my-scalatra-web-app",
     file("."),
-    settings = ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+    settings = ScalatraPlugin.scalatraSettings ++ scalateSettings ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
@@ -27,10 +26,8 @@ object MyScalatraWebAppBuild extends Build {
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % JettyVersion % "container",
-        "org.eclipse.jetty" % "jetty-plus" % JettyVersion % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0"
+        "ch.qos.logback" % "logback-classic" % "1.1.3" % "runtime",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
