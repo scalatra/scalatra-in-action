a023ea8c5a90b31fa770e9c36fb1fde7d31197c0
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-05-26 19:55:10 +0200

    Removed template settings (included in scalatra-scalate).

--- chapter02/src/main/scala/com/example/cms/ScalatraCmsStack.scala
+++ chapter02/src/main/scala/com/example/cms/ScalatraCmsStack.scala
@@ -1,36 +1,10 @@
 package com.example.cms
 
-import collection.mutable
-import javax.servlet.http.HttpServletRequest
-import org.fusesource.scalate.TemplateEngine
-import org.fusesource.scalate.layout.DefaultLayoutStrategy
 import org.scalatra._
 import scalate.ScalateSupport
 
 trait ScalatraCmsStack extends ScalatraServlet with ScalateSupport {
   
-  /* wire up the precompiled templates */
-  override protected def defaultTemplatePath: 
-    List[String] = List("/templates/views")
-
-  override protected def createTemplateEngine(config: ConfigT) = {
-    val engine = super.createTemplateEngine(config)
-    engine.layoutStrategy = new DefaultLayoutStrategy(engine,
-      TemplateEngine.templateTypes.map(
-        "/templates/layouts/default." + _): _*)
-
-    engine.packagePrefix = "templates"
-    engine
-  }
-  /* end wiring up the precompiled templates */
-  
-  override protected def templateAttributes(implicit request:
-    HttpServletRequest): mutable.Map[String, Any] = {
-      // Add extra attributes here, they need bindings in the build file
-      super.templateAttributes ++ mutable.Map.empty 
-  }
-  
-
   notFound {
     // remove content type in case it was set through an action
     contentType = null
@@ -40,4 +14,5 @@ trait ScalatraCmsStack extends ScalatraServlet with ScalateSupport {
       layoutTemplate(path)
     } orElse serveStaticResource() getOrElse resourceNotFound()
   }
+
 }
