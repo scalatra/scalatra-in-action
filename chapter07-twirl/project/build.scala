0ce6e637261412fda31d9519c60d83499570f6f4
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-05-26 19:24:18 +0200

    Updated build.

--- chapter07-twirl/project/build.scala
+++ chapter07-twirl/project/build.scala
@@ -1,3 +1,4 @@
+import org.scalatra.sbt.ScalatraPlugin
 import sbt._
 import Keys._
 import play.twirl.sbt.SbtTwirl
@@ -8,51 +9,24 @@ object Chapter07TwirlBuild extends Build {
   val Version = "0.1.0-SNAPSHOT"
   val ScalaVersion = "2.11.6"
   val ScalatraVersion = "2.4.0.RC1"
-  val JettyVersion = "9.1.3.v20140225"
-
-  import java.net.URL
-  import com.earldouglas.xsbtwebplugin.PluginKeys.port
-  import com.earldouglas.xsbtwebplugin.WebPlugin.{container, webSettings}
-
-  def Conf = config("container")
-
-  def jettyPort = 8080
 
   lazy val project = Project (
     Name,
     file("."),
-    settings = webSettings ++ Seq(
+    settings = Seq(
       organization := Organization,
       name := Name,
       version := Version,
       scalaVersion := ScalaVersion,
-      port in Conf := jettyPort,
       resolvers += Classpaths.typesafeReleases,
-      resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % JettyVersion % "container",
-        "org.eclipse.jetty" % "jetty-plus" % JettyVersion % "container",
+        "ch.qos.logback" % "logback-classic" % "1.1.3" % "runtime",
         "javax.servlet" % "javax.servlet-api" % "3.1.0"
-      ),
-      browseTask
+      )
     )
-  ).enablePlugins(SbtTwirl) 
+  ).settings(ScalatraPlugin.scalatraSettings:_*)
+   .enablePlugins(SbtTwirl)
 
-  val browse = TaskKey[Unit]("browse", "open web browser to localhost on container:port")
-  val browseTask = browse <<= (streams, port in container.Configuration) map { (streams, port) =>
-    import streams.log
-    val url = new URL("http://localhost:%s" format port)
-    try {
-      log info "Launching browser."
-      java.awt.Desktop.getDesktop.browse(url.toURI)
-    }
-    catch {
-      case t: Throwable => {
-        log info { "Could not open browser, sorry. Open manually to %s." format url.toExternalForm }
-      }
-    }
-  }
 }
--- chapter07-twirl/project/plugins.sbt
+++ chapter07-twirl/project/plugins.sbt
@@ -1,5 +1,3 @@
-addSbtPlugin("com.mojolly.scalate" % "xsbt-scalate-generator" % "0.5.0")
-
-addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.5")
+addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.4.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-twirl" % "1.0.4")
