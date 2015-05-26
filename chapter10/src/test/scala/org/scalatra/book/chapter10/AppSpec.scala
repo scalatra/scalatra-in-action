b1786be5a2735ad76e3e39aec0f336281be46c54
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2015-05-26 14:11:07 +0100

    A test for Chapter 10.

---
+++ chapter10/src/test/scala/org/scalatra/book/chapter10/AppSpec.scala
@@ -0,0 +1,27 @@
+package org.scalatra.book.chapter10
+
+import slick.driver.H2Driver.api._
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.scalatra.test.scalatest.ScalatraSuite
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class AppSpec extends FunSuite with ScalatraSuite with BeforeAndAfter {
+
+  val jdbcUrl = "jdbc:h2:mem:chapter10;DB_CLOSE_DELAY=-1"
+  val jdbcDriverClass = "org.h2.Driver"
+  val db = Database.forURL(jdbcUrl, driver = jdbcDriverClass)
+  val res = db.run(DbSetup.createDatabase)
+
+  Await.result(res, Duration(2, "seconds"))
+
+  addServlet(new Chapter10App(db), "/*")
+
+  test("simple get") {
+    get("/areas") {
+      status should equal(200)
+    }
+  }
+
+}
