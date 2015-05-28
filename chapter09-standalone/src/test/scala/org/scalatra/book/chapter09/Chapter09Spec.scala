043e827f8a4c69c157b848d48c2720c35729adad
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-05-28 20:35:37 +0200

    Updated test.

--- chapter09-standalone/src/test/scala/org/scalatra/book/chapter09/Chapter09Spec.scala
+++ chapter09-standalone/src/test/scala/org/scalatra/book/chapter09/Chapter09Spec.scala
@@ -2,23 +2,39 @@ package org.scalatra.book.chapter09
 
 import org.scalatra.test.specs2._
 
-class Chapter09Spec extends ScalatraSpec { def is =
-  "Chapter09"                     ^
-    "/ should should execute an action"         ! action ^
-    "/static.txt should return static file"     ! staticFile ^
-                                                end
+class Chapter09Spec extends MutableScalatraSpec {
 
   val conf = AppConfig.load
+  sys.props(org.scalatra.EnvironmentKey) = AppEnvironment.asString(conf.env)
 
   addServlet(new Chapter09(conf), "/*")
 
-  def action = get("/") {
-    status must_== 200
+  "/ should should execute an action" in {
+    get("/") {
+      status must_== 200
+    }
   }
 
-  def staticFile = get("/static.txt") {
-    status must_== 200
-    body must_== "this is static text!"
+  "/shorten-url should should execute an action" in {
+    get("/shorten-url") {
+      status must_== 200
+    }
+  }
+
+  "/static.txt should return static file" in {
+    get("/static.txt") {
+      status must_== 200
+      body must_== "this is static text!"
+    }
+  }
+
+  "/hello-scalate should render a template" in {
+    get("/hello-scalate") {
+      status must_== 200
+      body must_==
+        """<p>Hello, Scalate!</p>
+          |""".stripMargin
+    }
   }
 
 }
