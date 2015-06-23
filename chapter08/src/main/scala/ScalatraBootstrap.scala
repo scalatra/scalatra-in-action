0d71171172b0ede00a447384e5d089dd50799151
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2015-06-23 12:37:13 +0100

    Mounting the FoodServlet.

--- chapter08/src/main/scala/ScalatraBootstrap.scala
+++ chapter08/src/main/scala/ScalatraBootstrap.scala
@@ -5,6 +5,7 @@ import org.scalatra.book.chapter08._
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    context.mount(new FoodServlet, "/*")
     context.mount(new MyScalatraServlet, "/*")
     context.mount(new NukeLauncherServlet(RealNukeLauncher), "/nuke/*")
   }
