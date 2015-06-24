20c9d2c86c4a0fba2450f285bc606cb8e42efb9d
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2015-06-24 11:33:08 +0100

    Adding the cookie-handling example code from the chapter text.

--- chapter04/src/main/scala/ScalatraBootstrap.scala
+++ chapter04/src/main/scala/ScalatraBootstrap.scala
@@ -4,6 +4,7 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    context.mount(new CookiesExample, "/cookies/*")
     context.mount(new HackersController, "/*")
   }
 }
---
+++ chapter04/src/main/scala/com/constructiveproof/hackertracker/CookiesExample.scala
@@ -0,0 +1,17 @@
+package com.constructiveproof.hackertracker
+
+import org.scalatra._
+
+class CookiesExample extends ScalatraServlet {
+
+  get("/") {
+    val previous = cookies.get("counter") match {
+      case Some(v) =>  v.toInt
+      case None    => 0
+    }
+    cookies.update("counter", (previous+1).toString)
+    <p>
+      Hi, you have been on this page {previous} times already
+    </p>
+  }
+}
