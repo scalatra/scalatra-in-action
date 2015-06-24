da9bc6cb5bf0f200d28a66808a701717e6f9ce97
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2015-06-24 11:32:52 +0100

    Adding printlns to make things a bit easier to understand.

--- chapter04/src/main/scala/com/constructiveproof/hackertracker/HackersController.scala
+++ chapter04/src/main/scala/com/constructiveproof/hackertracker/HackersController.scala
@@ -32,21 +32,35 @@ class HackersController extends HackerTrackerStack {
     val birthYear = params.getAs[Int]("birth-year").getOrElse(
       halt(BadRequest("Please provide a year of birth.")))
 
+    println(name)
+    println(motto)
+
+    if(birthYear >= 1970) {
+      println("Adding a hacker who was born within the Unix epoch.")
+    } else {
+      println("Adding a classical hacker.")
+    }
+
     // create a new hacker and redirect to /hackers/:slug
   }
 
   get("/hackers/:slug") {
     val slug = params("slug")
+    println(slug)
     // retrieve and display info about this hacker
   }
 
   get("/results") {
     val searchQuery = params("search_query")
+    val originalQuery = params("oq")
+    println(searchQuery)
+    println(originalQuery)
     // search for and display matching hackers
   }
 
   get("/hackers/tagged") {
     val tags = multiParams("tag")
+    println(tags)
     // retrieve and display hackers matching all the given tags
   }
 }
