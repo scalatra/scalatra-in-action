1b787018126e10a002a84e5b69933ab0183191ad
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2015-07-01 10:34:45 +0100

    Removing JSONP - running this code doesn't give the response indicated in the current book draft.

--- chapter05/src/main/scala/org/scalatra/book/chapter05/MyJsonApp.scala
+++ chapter05/src/main/scala/org/scalatra/book/chapter05/MyJsonApp.scala
@@ -3,16 +3,9 @@ package org.scalatra.book.chapter05
 import org.json4s._
 import org.scalatra._
 
-class MyJsonApp extends ScalatraServlet with MyJsonRoutes with MyFoodRoutes with MyJsonpRoutes {
+class MyJsonApp extends ScalatraServlet with MyJsonRoutes with MyFoodRoutes {
 
   implicit lazy val jsonFormats = DefaultFormats +
     new NutritionFactsSerializer
 
 }
-
-
-
-
-
-
-
